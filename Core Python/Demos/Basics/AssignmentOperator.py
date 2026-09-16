660e2e9385751829a19920129f4a5b279c2a1ac2
Harshal1717 <harshalborude13201712@gmail.com>
2026-09-16 19:25:39 +0530

    Add files via upload

---
+++ Core Python/Demos/Basics/AssignmentOperator.py
@@ -0,0 +1,36 @@
+#1 =
+num = 20
+print(num)
+
+#2 +=
+num += 10
+print(num)
+
+#3 -=
+num -= 2
+print(num)
+
+#4 *=
+num *= 2
+print(num)
+
+#5 /=
+num /= 2
+print(num)
+
+#6 //=
+
+num //= 2
+print(num)
+
+#7 %
+
+num %= 4
+print(num)
+
+#8 
+
+num **= 2
+print(num)
+
+
